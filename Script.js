da784840d3caaa3bd611c5864f793bb5dec60f34
Alvin Alvrahesta <alvin.alvrahesta@gmail.com>
2021-09-05 21:13:15 +0700

    Initial commit

---
+++ Script.js
@@ -0,0 +1,12 @@
+var Panjang, Lebar, Keliling, Luas;
+
+hitungLuasKeliling = () => {
+  Panjang = parseInt(document.getElementById("panjang").value);
+  Lebar = parseInt(document.getElementById("lebar").value);
+  Keliling = 2 * (Panjang + Lebar);
+  Luas = Panjang * Lebar;
+  document.getElementById("keliling").value =
+    Keliling + (Keliling % 2 == 0 ? " (Genap)" : " (Ganjil)");
+  document.getElementById("luas").value =
+    Luas + (Luas % 2 == 0 ? " (Genap)" : " (Ganjil)");
+};
