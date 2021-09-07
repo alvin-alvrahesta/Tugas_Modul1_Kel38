4c1b6d68d3f476d0964d139c9c0e4b9da5e60f39
Alvin Alvrahesta <alvin.alvrahesta@gmail.com>
2021-09-07 23:49:07 +0700

    Fix Semua Syarat

--- Script.js
+++ Script.js
@@ -1,13 +1,27 @@
+//Menampilkan Nama Kelompok Menggunakan Function Map (Pengolahan Array)
+const anggota = [
+  { nama: "Alvin Alvrahesta", nim: "21120118120025" },
+  { nama: "Hilal Haniefam", nim: "21120118130047" },
+];
+
+document.getElementById("textkelompok").innerHTML = anggota.map(getKelompok);
+
+function getKelompok(item) {
+  return [item.nama, item.nim].join(" - ");
+}
+
+//Fungsi Asynchronous
 var Panjang, Lebar, Keliling, Luas;
 const fungsiAsyncAwait = async () => {
   await keterangan();
   await hitungLuasKeliling();
-}
+};
 
-const keterangan=()=>{
+const keterangan = () => {
   console.log("Hasil perhitungan: ");
-}
+};
 
+//Menghitung dan Menampilkan Hasil Perhitungan
 const hitungLuasKeliling = () => {
   Panjang = parseInt(document.getElementById("panjang").value);
   Lebar = parseInt(document.getElementById("lebar").value);
@@ -16,16 +30,20 @@ const hitungLuasKeliling = () => {
 
   document.getElementById("keliling").value =
     Keliling + (Keliling % 2 == 0 ? " (Genap)" : " (Ganjil)");
+
+  //Hasil Keliling juga ditampilkan pada Console.log
   console.log(
     "Keliling = " + Keliling + (Keliling % 2 == 0 ? " (Genap)" : " (Ganjil)")
   );
 
   document.getElementById("luas").value =
     Luas + (Luas % 2 == 0 ? " (Genap)" : " (Ganjil)");
+
+  //Hasil Luas juga ditampilkan juga pada Console.log
   console.log("Luas = " + Luas + (Luas % 2 == 0 ? " (Genap)" : " (Ganjil)"));
 };
 
 function reset() {
   document.getElementById("panjang").value = "";
   document.getElementById("lebar").value = "";
- }
+}
