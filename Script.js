f1bf5de1bd97d1259c336b7fb53e8c16ddfdafa8
Alvin Alvrahesta <alvin.alvrahesta@gmail.com>
2021-09-07 22:46:29 +0700

    + Syarat 5 Console Log

--- Script.js
+++ Script.js
@@ -5,8 +5,14 @@ hitungLuasKeliling = () => {
   Lebar = parseInt(document.getElementById("lebar").value);
   Keliling = 2 * (Panjang + Lebar);
   Luas = Panjang * Lebar;
+
   document.getElementById("keliling").value =
     Keliling + (Keliling % 2 == 0 ? " (Genap)" : " (Ganjil)");
+  console.log(
+    "Keliling = " + Keliling + (Keliling % 2 == 0 ? " (Genap)" : " (Ganjil)")
+  );
+
   document.getElementById("luas").value =
     Luas + (Luas % 2 == 0 ? " (Genap)" : " (Ganjil)");
+  console.log("Luas = " + Luas + (Luas % 2 == 0 ? " (Genap)" : " (Ganjil)"));
 };
